2f0c0b43e37ee2e1d4f8543a0dcb912533f53e52
Hyoungseok <khs96p@gmail.com>
2022-08-10 17:48:57 +0900

    dd

---
+++ client.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+class Client:
+    def __init__(self, user):
+        self.user=user # pandas Series 형태로
+
+
--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-print("test")
-print("dd")
+import pandas as pd
+import numpy as np
+import client
+import similarity_func
+import validation
+
+breeds=pd.read_csv('breeds.csv',index_col=0)
+print(breeds.head())
